f6ad565e65574759cf955eff353604a5c3d212fe
my9074 <313231068@qq.com>
2017-06-07 14:54:16 +0800

    Init store and style files

--- src/views/layout/AppMain.vue
+++ src/views/layout/AppMain.vue
@@ -1,9 +1,7 @@
 <template>
-  <section class="app-main" style="min-height: 100%">
-    <transition name="fade" mode="out-in">
-      <router-view :key="key"></router-view>
-    </transition>
-  </section>
+  <transition name="fade" mode="out-in">
+    <router-view :key="key"></router-view>
+  </transition>
 </template>
 
 <script>
--- src/views/layout/Breadcrumb.vue
+++ src/views/layout/Breadcrumb.vue
@@ -1,10 +1,10 @@
 <template>
-  <el-breadcrumb class="app-levelbar" separator="/">
-    <el-breadcrumb-item v-for="(item,index)  in levelList" :key="item">
-      <router-link v-if='item.redirect==="noredirect"||index==levelList.length-1' to="" class="no-redirect">{{item.name}}</router-link>
-      <router-link v-else :to="item.path">{{item.name}}</router-link>
-    </el-breadcrumb-item>
-  </el-breadcrumb>
+    <el-breadcrumb class="app-levelbar" separator="/">
+      <el-breadcrumb-item v-for="(item,index)  in levelList" :key="item">
+        <router-link v-if='item.redirect==="noredirect"||index==levelList.length-1' to="" class="no-redirect">{{item.name}}</router-link>
+        <router-link v-else :to="item.path">{{item.name}}</router-link>
+      </el-breadcrumb-item>
+    </el-breadcrumb>
 </template>
 
 <script>
