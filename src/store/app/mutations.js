d22807d2b756e37f564d1610059e3b5e202f0c6f
my9074 <313231068@qq.com>
2017-06-16 21:29:50 +0800

    Remov COUNT_APPS_HEALTHY type and enhance FETCH_APPS

--- src/store/app/mutations.js
+++ src/store/app/mutations.js
@@ -3,15 +3,12 @@ import {transformEnvtoArray, APP_BASE, countHealthyState} from '@/utils/app'
 import _ from 'lodash'
 
 export default {
-  [type.FETCH_APPS] (state, payload = []) {
-    state.apps.apps = payload
+  [type.FETCH_APPS] (state, apps = []) {
+    state.apps.apps = apps.map(app => countHealthyState(app))
     state.apps.total = state.apps.apps.length
   },
   [type.FETCH_APP] (state, payload = {}) {
     state.app = _.merge({}, APP_BASE, payload)
     state.app.envs = transformEnvtoArray(state.app.env)
-  },
-  [type.COUNT_APPS_HEALTHY] (state, apps = []) {
-    state.apps.apps = apps.map(app => countHealthyState(app))
   }
 }
--- src/store/app/mutations_types.js
+++ src/store/app/mutations_types.js
@@ -1,3 +1,2 @@
 export const FETCH_APPS = 'apps'
 export const FETCH_APP = 'app'
-export const COUNT_APPS_HEALTHY = 'COUNT_APPS_HEALTHY'
