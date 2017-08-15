58588d92e7544555047ae08f39e391366638e1e0
my9074 <313231068@qq.com>
2017-08-15 16:06:21 +0800

    Modify the deployment path for Production ENV

--- config/index.js
+++ config/index.js
@@ -7,9 +7,9 @@ module.exports = {
     port: 9000,
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'dataman-ui',
     assetsPublicPath: '/',
-    staticPath:'/static/', //生产环境 staticPath:''
+    staticPath:'/dataman-ui/', //生产环境 staticPath:''
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
