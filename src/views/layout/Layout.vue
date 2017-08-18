bcd2a9fd8e6f94ec6c0eec19577bb32f2d48a317
my9074 <313231068@qq.com>
2017-08-18 17:01:42 +0800

    1.Refactoring page structure 2.Add the login screen

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,36 +1,53 @@
 <template>
   <div class="app-wrapper">
+    <Sidebar class="sidebar-container"></Sidebar>
     <section class="main-content">
-      <Tabs>
-        <App-main/>
-      </Tabs>
+      <Breadcrumb class="bread-header"></Breadcrumb>
+      <App-main/>
     </section>
   </div>
 </template>
 
 <script>
-import { Breadcrumb, AppMain, Tabs } from './index'
+import { Breadcrumb, AppMain, Sidebar } from './index'
 
 export default {
   name: 'layout',
   components: {
     Breadcrumb,
     AppMain,
-    Tabs
+    Sidebar
   }
 }
 </script>
+
 <style scoped>
+.app-wrapper {
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.sidebar-container {
+  width: 180px;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1001;
+  overflow-y: auto;
+}
+
 .bread-header {
-  display: flex;
   width: 100%;
   height: 50px;
-  background: #2c867a;
   line-height: 50px;
-  align-items: center;
 }
 
 .main-content {
+  min-height: 100%;
+  margin-left: 180px;
   padding: 20px;
 }
 </style>
--- src/views/layout/index.js
+++ src/views/layout/index.js
@@ -3,3 +3,5 @@ export { default as Breadcrumb } from './Breadcrumb'
 export { default as AppMain } from './AppMain'
 
 export { default as Tabs } from './Tabs'
+
+export { default as Sidebar } from './Sidebar'
