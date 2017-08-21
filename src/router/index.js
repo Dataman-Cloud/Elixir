0f7d6c0b7ab773298cd27808f8c318349c94d175
my9074 <313231068@qq.com>
2017-08-21 16:00:14 +0800

    Add permission to verify the initial logic

--- src/router/index.js
+++ src/router/index.js
@@ -116,8 +116,7 @@ export const asyncRouterMap = [
     children: [
       { path: 'list', component: LogList, name: '日志查询' }
     ]
-  },
-  { path: '*', redirect: '/app', hidden: true }
+  }
 ]
 
 const router = new Router({
