a6768cf9509ebfafc1f0d03749e81afdd83ad624
my9074 <313231068@qq.com>
2017-08-21 10:33:55 +0800

    Remove useid/projectid/bayname flag

--- src/store/user/mutations.js
+++ src/store/user/mutations.js
@@ -1,9 +1,5 @@
 import * as type from './mutations_types'
 
 export default {
-  [type.SET_USER_INFO] (state, {userid, projectid, bayname}) {
-    state.userid = userid
-    state.projectid = projectid
-    state.bayname = bayname
-  }
+  [type.SET_USER_INFO] (state, payload) {}
 }
