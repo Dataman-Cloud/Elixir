c75dfe60891bd6b6018c0c2b7c0f402bcddb361f
lixiaoyanqq <lixy_9791@163.com>
2017-08-24 16:30:48 +0800

    The cluster module

---
+++ src/views/cluster/modals/AddDialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-dialog title="添加主机" v-model="dialogVisible" size="tiny" ref="dialog">
+    <div>
+      编辑集群
+    </div>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    open: function () {
+      this.$refs.dialog.open()
+    }
+  }
+}
+</script>
