c9775d4be24f9296b3346e01bebe0597d0240bef
my9074 <313231068@qq.com>
2017-08-25 15:48:14 +0800

    Initialize the user group management menu

---
+++ src/views/system/user-group/list/List.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="btn-group">
+      <span>
+        <el-button type="primary">
+          <i class="ion-ios-plus-outline"></i> 新建用户组</el-button>
+      </span>
+      <el-button-group style="display: flex">
+        <el-input class="el-input-search" icon="search" v-model="searchWord" placeholder="请输入内容"></el-input>
+      </el-button-group>
+    </div>
+
+    <el-table ref="multipleTable" :data="tableData3" border v-loading="listLoading" tooltip-effect="dark" style="width: 100%" @selection-change="handleCurrentChange">
+      <el-table-column type="selection" width="55">
+      </el-table-column>
+      <el-table-column label="日期" width="120">
+        <template scope="scope">{{ scope.row.date }}</template>
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120">
+      </el-table-column>
+      <el-table-column prop="address" label="地址" show-overflow-tooltip>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      listLoading: false,
+      searchWord: '',
+      tableData3: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-08',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-07',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }]
+    }
+  },
+  methods: {
+    handleCurrentChange (val) { },
+    reload () { }
+  },
+  created () { }
+}
+
+</script>
+<style scoped>
+.btn-group {
+  justify-content: space-between;
+}
+</style>
