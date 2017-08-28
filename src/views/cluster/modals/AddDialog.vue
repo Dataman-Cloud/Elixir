af530b2ca66db0845ea17ebfcb435a3d733d95f5
刘海洋 <liuhaiyang1992@hotmail.com>
2017-08-28 17:58:31 +0800

    Intergration (#178)
    * Add some getters for store and remove useless getters
    
    * ci($git): Add cz-cli (#174)
    
    Add the cz-cli to enhance the commit message
    
    * The cluster create cluster (#175)
    
    * fix: Fix the problem that sometimes navigation can't jump (#176)
    
    * octopus create html (#172)
    
    * minihost html (#177)

--- src/views/cluster/modals/AddDialog.vue
+++ src/views/cluster/modals/AddDialog.vue
@@ -11,15 +11,65 @@
 </template>
 
 <script>
+import * as cluster from '@/api/cluster'
+import * as type from '@/store/user-group/mutations_types'
+import { mapActions } from 'vuex'
 export default {
   data () {
     return {
-      dialogVisible: false
+      loading: false,
+      dialogVisible: false,
+      submitLoading: false,
+      userGroups: [],
+      form: {
+        clusterLabel: '',
+        desc: ''
+      },
+      rules: {
+        clusterLabel: [
+          { required: true, message: '集群名称不能为空' }
+        ],
+        userGroup: [
+          { required: true, message: '请选择所属用户组' }
+        ]
+      }
     }
   },
   methods: {
+    ...mapActions({
+      fetchUserGroups: type.FETCH_USER_GROUPS
+    }),
+    close () {
+      this.resetForm()
+      this.submitLoading = false
+    },
     open: function () {
       this.$refs.dialog.open()
+    },
+    async openUserGroup () {
+      let data = await this.fetchUserGroups()
+      this.userGroups = data.data
+    },
+    onSubmit () {
+      this.$refs.form.validate(async (valid) => {
+        if (valid) {
+          this.submitLoading = true
+          try {
+            await cluster.create(this.form)
+            this.dialogVisible = false
+            this.$store.dispatch(type.FETCH_CLUSTERS)
+            this.loading = false
+          } catch (error) {
+            this.submitLoading = false
+          }
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm () {
+      this.$refs.form.resetFields()
     }
   }
 }
