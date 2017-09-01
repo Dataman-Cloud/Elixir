ed6477962a0b0de0916d7aaa56fb2a45f425c1ed
Myu <313231068@qq.com>
2017-09-01 15:10:08 +0800

    docs(compose):Basic json change (#212) (#213)

--- src/views/compose/services/composeTemplate.js
+++ src/views/compose/services/composeTemplate.js
@@ -1,7 +1,7 @@
 export const COMPOSE_BASE = {
-  name: 'wordpressbp',
-  desc: 'wordpressbp',
-  'yaml_raw': 'version: \'3\'\n \nservices:\n  wordpress: \n    image: \'wordpress\'\n    network_mode: \'bridge\'\n    environment: \n      - WORDPRESS_DB_HOST=db:3306\n      - WORDPRESS_DB_PASSWORD=Password\n    ports: \n      - 8080:80\n    depends_on:\n      - wordpressdb\n    logging:\n      options:\n        env: SWAN_TASK_ID,SWAN_TASK_NAME,SWAN_PORTS,SWAN_HOST,SWAN_PORT0,SWAN_APP_ID,SWAN_APP_VERSION\n        labels: JBORG_VCLUSTER,JBORG_USER_ID,JBORG_GROUP_ID,BEAT_EXCLUDE,JBORG_APP_ID\n\n  wordpressdb:\n    image: \'mariadb\'\n    network_mode: \'host\'\n    environment:\n      - MYSQL_ROOT_PASSWORD=Password\n    logging: \n      options:\n        env: SWAN_TASK_ID,SWAN_TASK_NAME,SWAN_PORTS,SWAN_HOST,SWAN_PORT0,SWAN_APP_ID,SWAN_APP_VERSION\n        labels: JBORG_VCLUSTER,JBORG_USER_ID,JBORG_GROUP_ID,BEAT_EXCLUDE,JBORG_APP_ID\n',
+  'name': 'wordpressbp',
+  'desc': 'wordpressbp',
+  'yaml_raw': 'version: "3"\n \nservices:\n  wordpress: \n    image: "wordpress"\n    network_mode: "bridge"\n    environment: \n      - WORDPRESS_DB_HOST=db:3306\n      - WORDPRESS_DB_PASSWORD=Password\n    ports: \n      - 8080:80\n    depends_on:\n      - wordpressdb\n    logging:\n      options:\n        env: SWAN_TASK_ID,SWAN_TASK_NAME,SWAN_PORTS,SWAN_HOST,SWAN_PORT0,SWAN_APP_ID,SWAN_APP_VERSION\n        labels: JBORG_VCLUSTER,JBORG_USER_ID,JBORG_GROUP_ID,BEAT_EXCLUDE,JBORG_APP_ID\n\n  wordpressdb:\n    image: "mariadb"\n    network_mode: "host"\n    environment:\n      - MYSQL_ROOT_PASSWORD=Password\n    logging: \n      options:\n        env: SWAN_TASK_ID,SWAN_TASK_NAME,SWAN_PORTS,SWAN_HOST,SWAN_PORT0,SWAN_APP_ID,SWAN_APP_VERSION\n        labels: JBORG_VCLUSTER,JBORG_USER_ID,JBORG_GROUP_ID,BEAT_EXCLUDE,JBORG_APP_ID\n',
   'yaml_extra': {
     'wordpress': {
       'runas': 'bbk',
@@ -20,6 +20,12 @@ export const COMPOSE_BASE = {
       'resource': {
         'cpus': 0.01,
         'mem': 64
+      },
+      'proxy': {
+        'enabled': true,
+        'alias': 'g1.cn',
+        'listen': 991,
+        'sticky': false
       },
       'labels': {
         'JBORG_APP_ID': 'wordpress',
@@ -47,6 +53,12 @@ export const COMPOSE_BASE = {
       'resource': {
         'cpus': 0.01,
         'mem': 64
+      },
+      'proxy': {
+        'enabled': true,
+        'alias': 'g2.cn',
+        'listen': 992,
+        'sticky': false
       },
       'labels': {
         'JBORG_APP_ID': 'wordpressdb',
