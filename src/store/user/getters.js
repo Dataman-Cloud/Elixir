21cc12ad38b849d51577edd5201aadff393f1e41
Myu <313231068@qq.com>
2017-09-12 11:55:33 +0800

    feat(state): Add isPlateform/isOwner/isTenant/isMember flag to user getter (#238)

--- src/store/user/getters.js
+++ src/store/user/getters.js
@@ -1,5 +1,29 @@
+function findCurGroup (groups = [], curGroupId) {
+  return groups.find(group => group.groupId === curGroupId)
+}
+
 export default {
   currentGroupId: state => state.currentGroupId,
   currentGroupName: state => state.currentGroupName,
-  isCollapse: state => state.isCollapse
+  isCollapse: state => state.isCollapse,
+  // 平台管理员
+  isPlatform: state => {
+    let curGroup = findCurGroup(state.accountGroups, state.currentGroupId)
+    return curGroup ? curGroup.role === 'plateform' : false
+  },
+  // 组管理员
+  isOwner: state => {
+    let curGroup = findCurGroup(state.accountGroups, state.currentGroupId)
+    return curGroup ? curGroup.role === 'owner' : false
+  },
+  // 租户
+  isTenant: state => {
+    let curGroup = findCurGroup(state.accountGroups, state.currentGroupId)
+    return curGroup ? curGroup.role === 'superuser' : false
+  },
+  // 普通成员
+  isMember: state => {
+    let curGroup = findCurGroup(state.accountGroups, state.currentGroupId)
+    return curGroup ? curGroup.role === 'member' : false
+  }
 }
