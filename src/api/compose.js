1b5aba28cccbf5b3f368d648a29a3e0860bf9630
my9074 <313231068@qq.com>
2017-09-13 18:01:05 +0800

    fix(compose): Modify compose api

--- src/api/compose.js
+++ src/api/compose.js
@@ -2,28 +2,28 @@ import fetch from '@/utils/fetch'
 
 export function listCompose () {
   return fetch({
-    url: 'v1/composes',
+    url: 'v1/composes-ng',
     method: 'get'
   })
 }
 
 export function getCompose (id) {
   return fetch({
-    url: `v1/composes/${id}`,
+    url: `v1/composes-ng/${id}`,
     method: 'get'
   })
 }
 
 export function deleteCompose (id) {
   return fetch({
-    url: `v1/composes/${id}`,
+    url: `v1/composes-ng/${id}`,
     method: 'delete'
   })
 }
 
 export function createCompose (data) {
   return fetch({
-    url: `v1/composes`,
+    url: `v1/composes-ng`,
     method: 'post',
     data: data
   })
