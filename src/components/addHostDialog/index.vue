3736dd38d1850eacaa7154ba8997fe88f02c816a
fenfenraorao <877956390@qq.com>
2017-09-14 11:34:34 +0800

    fix(host):The button is disabled when the item is not selected

--- src/components/addHostDialog/index.vue
+++ src/components/addHostDialog/index.vue
@@ -1,11 +1,11 @@
 <template>
   <el-dialog title="添加主机" v-model="dialogVisible" size="small" ref="dialog">
     <div>
-      <el-transfer v-model="checkedHost" :data="hostList" :titles="['选择主机','已选主机']"></el-transfer>
+      <el-transfer v-model="rightHosts" :data="hostList" :titles="['选择主机','已选主机']"></el-transfer>
     </div>
     <div slot="footer" class="dialog-footer">
       <el-button @click="dialogVisible = false">取 消</el-button>
-      <el-button type="primary" @click="addHost" :disabled="checkedHost.length == 0">确 定</el-button>
+      <el-button type="primary" @click="addHost" :disabled="rightHosts.length === 0">确 定</el-button>
     </div>
   </el-dialog>
 </template>
@@ -15,7 +15,7 @@ export default {
   data () {
     return {
       dialogVisible: false,
-      checkedHost: []
+      rightHosts: []
     }
   },
   props: {
@@ -29,10 +29,10 @@ export default {
   methods: {
     open: function () {
       this.$refs.dialog.open()
-      this.checkedHost = []
+      this.rightHosts = []
     },
     async addHost () {
-      await this.$emit('close', this.checkedHost)
+      await this.$emit('close', this.rightHosts)
       this.dialogVisible = false
     }
   }
