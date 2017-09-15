23b7bd33e68301f86710a9b7b7bc82c341bc7430
lixiaoyanqq <1175970889@qq.com>
2017-09-15 13:29:24 +0800

    feat(Dashboard): Add dashboard page (#257)
    feat(dashboard): drawing

---
+++ src/components/charts/Bar.vue
@@ -0,0 +1,94 @@
+<template>
+  <div :class="className" :id="id" :style="{height:height,width:width}"></div>
+</template>
+<script>
+  // 引入 ECharts 主模块
+  const echarts = require('echarts/lib/echarts')
+
+  require('echarts/lib/chart/bar')
+  // 引入提示框和标题组件
+  require('echarts/lib/component/tooltip')
+  require('echarts/lib/component/toolbox')
+  require('echarts/lib/component/title')
+  require('echarts/lib/component/legend')
+export default {
+    name: 'pie',
+    props: {
+      className: {
+        type: String,
+        default: 'chart'
+      },
+      id: {
+        type: String,
+        default: 'chart'
+      },
+      width: {
+        type: String,
+        default: '200px'
+      },
+      height: {
+        type: String,
+        default: '200px'
+      },
+      title: {
+        type: String,
+        default: ''
+      }
+    },
+    data () {
+      return {
+        chart: null
+      }
+    },
+    mounted () {
+      this.initChart()
+    },
+    methods: {
+      initChart () {
+        if (!this.chart) {
+          this.chart = echarts.init(document.getElementById(this.id))
+        }
+        this.chart.setOption({
+          color: ['#3398DB'],
+          title: {
+            text: this.title
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+              type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              type: 'category',
+              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+              axisTick: {
+                alignWithLabel: true
+              }
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value'
+            }
+          ],
+          series: [
+            {
+              name: '直接访问',
+              type: 'bar',
+              barWidth: '60%',
+              data: [10, 52, 200, 334, 390, 330, 220]
+            }
+          ]
+        })
+      }
+    }
+}
+</script>
--- src/views/home/dashboard/pages/owner.vue
+++ src/views/home/dashboard/pages/owner.vue
@@ -1,13 +1,102 @@
 <template>
-  <div>owner</div>
+  <div>
+    <el-row>
+      <el-col :span="6">
+        <el-card :body-style="{ padding: '0px' }">
+          <div style="padding: 14px;">
+            <div class="card-title">主机</div>
+            <div>
+              <pie-chart id="host" height='290px' width='100%' :legend="legend" :serieDatas="serieDatas"></pie-chart>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="6" :offset="2">
+        <el-card :body-style="{ padding: '0px' }">
+          <div style="padding: 14px;">
+            <div class="card-title">应用</div>
+            <div style="width:100%;height:290px;">
+              <h2 class="card-num">1</h2>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="6" :offset="2">
+        <el-card :body-style="{ padding: '0px' }">
+          <div style="padding: 14px;">
+            <div class="card-title">容器</div>
+            <div style="width:100%;height:290px;">
+              <h1 class="card-num">2</h1>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row style="margin-top:20px;">
+      <el-col :span="24">
+        <h1 class="dashboard-title">
+          资源使用量
+        </h1>
+      </el-col>
+      <el-col :span="10">
+        <el-card :body-style="{ padding: '0px' }">
+          <div style="padding: 14px;">
+            <div class="card-title">CPU使用量</div>
+            <bar-chart id="cpu" width='100%' height='300px'></bar-chart>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="10" :offset="2">
+        <el-card :body-style="{ padding: '0px' }">
+          <div style="padding: 14px;">
+            <div class="card-title">内存使用量</div>
+            <bar-chart id="mem" width='100%' height='300px'></bar-chart>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <script>
+import LineChart from '@/components/charts/Line'
+import PieChart from '@/components/charts/Pie'
+import BarChart from '@/components/charts/Bar'
+
 export default {
+  components: {
+    PieChart,
+    LineChart,
+    BarChart
+  },
   data () {
-    return {}
+    return {
+      currentDate: new Date(),
+      legend: ['正常数', '异常数', '健康'],
+      serieDatas: [
+        { value: 335, name: '正常数' },
+        { value: 310, name: '异常数' },
+        { value: 200, name: '健康' }
+      ],
+      title: '主机数',
+      bar: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      cpuSeries: [10, 52, 200, 334, 390, 330, 220]
+    }
   }
 }
 </script>
 <style scoped>
-
+.dashboard-title{
+  font-size: 20px;
+  height: 50px;
+  line-height: 24px;
+}
+.card-title{
+  font-size: 18px;
+  text-align: center;
+}
+.card-num{
+  line-height: 290px;
+  text-align: center;
+  font-size: 100px;
+}
 </style>
