c057562a422f5f3afc95a6e75e59b510d6f5a4b7
fenfenraorao <877956390@qq.com>
2017-09-25 18:31:49 +0800

    feat(layout):Increased rights management

---
+++ src/views/layout/modals/RightsManagementDialog.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-dialog title="权限管理" v-model="dialogVisible" size="small" ref="dialog">
+    <el-form ref="form" :model="form" label-width="50px">
+      <el-form-item label="角色" style="width:283px">
+        <el-select v-model="form.role" placeholder="请选择" @change="listLimits(form.role)">
+          <el-option v-for="(item,index) in roles" :key="index" :label="item.label" :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <div>
+      <el-transfer v-model="rightLimit" :data="limitsList" :titles="['选择权限','已有权限']"></el-transfer>
+    </div>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="addRights">确 定</el-button>
+    </div>
+
+  </el-dialog>
+</template>
+
+<script>
+import * as type from '@/api/layout'
+
+export default {
+  data () {
+    return {
+      dialogVisible: false,
+      limitsList: [],
+      rightLimit: [],
+      roles: [{
+        value: 'superuser',
+        label: '租户'
+      }, {
+        value: 'owner',
+        label: '组管理员'
+      }, {
+        value: 'member',
+        label: '组成员'
+      }],
+      form: {
+        role: 'superuser'
+      }
+    }
+  },
+  methods: {
+    async addRights () {
+      await type.updateLimits({role: this.form.role, resources: this.rightLimit})
+      this.dialogVisible = false
+      this.$notify({ message: '权限修改成功' })
+    },
+    async listLimits (name) {
+      let { data } = await type.limitsList(name)
+      this.rightLimit = data.resources.map(item => item.id)
+      this.limitsList = data.resourcesAll.map(item => {
+        return {
+          key: item.id,
+          label: item.resourceName + ':' + item.description
+        }
+      })
+    },
+    open () {
+      this.$refs.dialog.open()
+      this.listLimits(this.form.role)
+    }
+  }
+}
+</script>
