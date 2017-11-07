cd868907a8bc24750a7387adc7ae7f4b9153463b
fenfenraorao <877956390@qq.com>
2017-11-07 15:33:09 +0800

    fix(cookie):Not out after close the browser,open the link and may go into the system and operate

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,14 @@
-import Cookies from 'js-cookie'
 
 const TokenKey = 'token'
 
 export function getToken () {
-  return Cookies.get(TokenKey)
+  return sessionStorage.getItem(TokenKey)
 }
 
 export function setToken (token) {
-  return Cookies.set(TokenKey, token)
+  return sessionStorage.setItem(TokenKey, token)
 }
 
 export function removeToken () {
-  return Cookies.remove(TokenKey)
+  return sessionStorage.removeItem(TokenKey)
 }
