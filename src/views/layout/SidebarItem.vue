c7889528f839cae8489673f4622847f5c8d3566b
fenfenraorao <877956390@qq.com>
2017-11-23 13:52:36 +0800

    fix(compose):Modify the sidebar and home page styles

--- src/views/layout/SidebarItem.vue
+++ src/views/layout/SidebarItem.vue
@@ -2,20 +2,19 @@
   <div>
     <template v-for="(item, index) in routes">
       <el-menu-item :key="index" v-if="!item.hidden&&!item.hasDropdown&&item.children.length>0" :index="item.path+'/'+item.children[0].path">
-        <i :class="[item.icon, 'clearance']" aria-hidden="true"></i>
+        <i :class="[item.icon]" aria-hidden="true"></i>
         <span slot="title">{{item.name}}</span>
       </el-menu-item>
 
       <el-submenu :index="item.name" v-if="item.hasDropdown&&!item.hidden&&item.children.length" :key="index">
         <template slot="title">
-          <i v-if='item.icon' :class="[item.icon, 'clearance']" aria-hidden="true"></i>
+          <i v-if='item.icon' :class="[item.icon]" aria-hidden="true"></i>
           <span>{{item.name}}</span>
         </template>
         <template v-for="(child, childIndex) in item.children" v-if='!child.hidden'>
           <sidebar-item v-if='child.children&&child.children.length>0' :routes='[child]' :key="childIndex"> </sidebar-item>
-
           <el-menu-item v-else :index="item.path+'/'+child.path" :key="childIndex">
-            <i v-if='child.icon' :class="[child.icon, 'clearance']" aria-hidden="true"></i>
+            <i v-if='child.icon' :class="[child.icon]" aria-hidden="true"></i>
             <span>{{child.name}}</span>
           </el-menu-item>
         </template>
@@ -36,13 +35,20 @@ export default {
 </script>
 
 <style scoped>
-.clearance {
-  margin-right: 14px
-}
+
 .el-submenu .el-menu-item {
   height: 50px;
   line-height: 50px;
   padding: 0 45px;
   min-width: 160px;
+}
+
+.fa,.icon-minihost-menu {
+  display: inline-block;
+  width: 30px;
+}
+
+.fa-home:before {
+  font-size: 18px;
 }
 </style>
